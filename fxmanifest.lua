40b7fc3689fb7da593c1c963b2e1534081099ebf
roblox-afk <66645833+roblox-afk@users.noreply.github.com>
2022-09-06 12:24:50 +0200

    Fxmanifest Create

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'John Alex <roblox104roblox@gmail.com>'
+description 'Robbery Script For Tropical RP'
+version '1.0.0'
+
+client_scripts {
+  '/client/main.lua'
+}
+
+server_script {
+  '/server/main.lua'
+}
+
+shared_script {
+  'config.lua'
+}
